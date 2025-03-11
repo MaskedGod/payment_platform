ed084949648cc86697c9acada0cfa6a7b2752702
MaskedGod <codegobbledygook@gmail.com>
2025-03-11 20:50:26 +0400

    add development db in docker, healthcheck endpoint/handler

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.sql import text
+
+from app.db.database import get_db
+
 
 app = FastAPI()
 
@@ -6,3 +10,18 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "from payment-platform!"}
+
+
+@app.get("/health")
+async def health_check(db=Depends(get_db)):
+    try:
+        result = await db.execute(text("SELECT 1"))
+        if result.scalar() == 1:
+            return {"status": "healthy", "db_status": "reachable"}
+        else:
+            return {"status": "unhealthy", "db_status": "unexpected result"}
+    except Exception as e:
+        raise HTTPException(
+            status_code=503,
+            detail={"status": "unhealthy", "db_status": "unreachable", "error": str(e)},
+        )
