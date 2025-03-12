a159f31b78beb89262612fff956c2f5f3adcb505
MaskedGod <codegobbledygook@gmail.com>
2025-03-12 23:06:23 +0400

    add redis and rabbit to docker, add connection variables to settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -10,6 +10,12 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    PAYADMIT_AUTH_URL: str
+    PAYADMIT_API_URL: str
+    REDIS_HOST: str
+    REDIS_PORT: int
+    RABBITMQ_HOST: str
+    RABBITMQ_PORT: int
 
     @property
     def DATABASE_URL(self):
