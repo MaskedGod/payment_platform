2274c364470f87b698ab6e5bb6efaf54ae395424
MaskedGod <codegobbledygook@gmail.com>
2025-03-14 17:03:23 +0400

    add User, Payment models, create migration.

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,6 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    PAYADMIT_AUTH_URL: str
+    PAYADMIT_API_URL: str
+    API_KEY: str
+    SIGN_KEY: str
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
@@ -10,8 +14,6 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
-    PAYADMIT_AUTH_URL: str
-    PAYADMIT_API_URL: str
     REDIS_HOST: str
     REDIS_PORT: int
     RABBITMQ_HOST: str
--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.exc import SQLAlchemyError
+from fastapi import HTTPException, status
+
 from app.config.settings import settings
 
 
@@ -15,6 +18,12 @@ async def get_db():
     async with async_session() as session:
         try:
             yield session
+        except SQLAlchemyError:
+            await session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Database operation failed",
+            )
         finally:
             await session.close()
 
