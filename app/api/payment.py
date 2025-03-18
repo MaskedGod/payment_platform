916dbd15000d4a0c054f65f5aa0fe756ff61a6fb
MaskedGod <codegobbledygook@gmail.com>
2025-03-19 00:33:11 +0400

    feat: remake payment endpoint and service to be available only with JWT token

--- app/api/payment.py
+++ app/api/payment.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, HTTPException, Depends
+from app.auth.auth_service import AuthService
 from app.payments.payments_service import PayAdmitService
+from app.users.models import User
 
 router = APIRouter()
 
 
 @router.get("/payments")
-async def get_payments(payadmit_service: PayAdmitService = Depends()):
+async def get_payments(
+    user: User = Depends(AuthService.get_current_user),
+    payadmit_service: PayAdmitService = Depends(),
+):
     """
     Получает список всех платежей через API PayAdmit.
     Возвращает:
@@ -14,14 +19,17 @@ async def get_payments(payadmit_service: PayAdmitService = Depends()):
     - HTTPException: если произошла ошибка при выполнении запроса к API.
     """
     try:
-        return await payadmit_service.get_payments()
+        return await payadmit_service.get_payments(user)
     except HTTPException as e:
         raise e
 
 
 @router.post("/payments")
 async def create_payment(
-    amount: int, currency: str, payadmit_service: PayAdmitService = Depends()
+    amount: int,
+    currency: str,
+    user: User = Depends(AuthService.get_current_user),
+    payadmit_service: PayAdmitService = Depends(),
 ):
     """
     Создает новый платеж через API PayAdmit.
@@ -34,14 +42,17 @@ async def create_payment(
     - HTTPException: если произошла ошибка при создании платежа.
     """
     try:
-        return await payadmit_service.create_payment(amount, currency)
+        return await payadmit_service.create_payment(user, amount, currency)
     except HTTPException as e:
         raise e
 
 
 @router.post("/payouts")
 async def create_payout(
-    amount: int, currency: str, payadmit_service: PayAdmitService = Depends()
+    amount: int,
+    currency: str,
+    user: User = Depends(AuthService.get_current_user),
+    payadmit_service: PayAdmitService = Depends(),
 ):
     """
     Создает новую выплату через API PayAdmit.
@@ -54,13 +65,16 @@ async def create_payout(
     - HTTPException: если произошла ошибка при создании выплаты.
     """
     try:
-        return await payadmit_service.create_payout(amount, currency)
+        return await payadmit_service.create_payout(user, amount, currency)
     except HTTPException as e:
         raise e
 
 
-@router.post("/refunds")
-async def create_refund(payment_id: int, payadmit_service: PayAdmitService = Depends()):
+async def create_refund(
+    payment_id: int,
+    user: User = Depends(AuthService.get_current_user),
+    payadmit_service: PayAdmitService = Depends(),
+):
     """
     Создает возврат средств для конкретного платежа через API PayAdmit.
     Параметры:
@@ -71,13 +85,17 @@ async def create_refund(payment_id: int, payadmit_service: PayAdmitService = Dep
     - HTTPException: если произошла ошибка при создании возврата.
     """
     try:
-        return await payadmit_service.create_refund(payment_id)
+        return await payadmit_service.create_refund(user, payment_id)
     except HTTPException as e:
         raise e
 
 
 @router.post("/payouts/{payout_id}/confirm")
-async def confirm_payout(payout_id: int, payadmit_service: PayAdmitService = Depends()):
+async def confirm_payout(
+    payout_id: int,
+    user: User = Depends(AuthService.get_current_user),
+    payadmit_service: PayAdmitService = Depends(),
+):
     """
     Подтверждает выплату через API PayAdmit.
     Параметры:
@@ -88,13 +106,17 @@ async def confirm_payout(payout_id: int, payadmit_service: PayAdmitService = Dep
     - HTTPException: если произошла ошибка при подтверждении выплаты.
     """
     try:
-        return await payadmit_service.confirm_payout(payout_id)
+        return await payadmit_service.confirm_payout(user, payout_id)
     except HTTPException as e:
         raise e
 
 
 @router.get("/payments/{payment_id}/status")
-async def check_status(payment_id: int, payadmit_service: PayAdmitService = Depends()):
+async def check_status(
+    payment_id: int,
+    user: User = Depends(AuthService.get_current_user),
+    payadmit_service: PayAdmitService = Depends(),
+):
     """
     Проверяет статус конкретного платежа через API PayAdmit.
     Параметры:
@@ -105,13 +127,16 @@ async def check_status(payment_id: int, payadmit_service: PayAdmitService = Depe
     - HTTPException: если произошла ошибка при проверке статуса.
     """
     try:
-        return await payadmit_service.check_status(payment_id)
+        return await payadmit_service.check_status(user, payment_id)
     except HTTPException as e:
         raise e
 
 
 @router.get("/operations")
-async def get_operations(payadmit_service: PayAdmitService = Depends()):
+async def get_operations(
+    user: User = Depends(AuthService.get_current_user),
+    payadmit_service: PayAdmitService = Depends(),
+):
     """
     Получает список всех операций через API PayAdmit.
     Возвращает:
@@ -120,13 +145,16 @@ async def get_operations(payadmit_service: PayAdmitService = Depends()):
     - HTTPException: если произошла ошибка при получении списка операций.
     """
     try:
-        return await payadmit_service.get_operations()
+        return await payadmit_service.get_operations(user)
     except HTTPException as e:
         raise e
 
 
 @router.get("/balance")
-async def get_balance(payadmit_service: PayAdmitService = Depends()):
+async def get_balance(
+    user: User = Depends(AuthService.get_current_user),
+    payadmit_service: PayAdmitService = Depends(),
+):
     """
     Получает текущий баланс через API PayAdmit.
     Возвращает:
@@ -135,44 +163,6 @@ async def get_balance(payadmit_service: PayAdmitService = Depends()):
     - HTTPException: если произошла ошибка при получении баланса.
     """
     try:
-        return await payadmit_service.get_balance()
-    except HTTPException as e:
-        raise e
-
-
-@router.post("/blacklist")
-async def update_blacklist(
-    blacklist: list, payadmit_service: PayAdmitService = Depends()
-):
-    """
-    Обновляет черный список через API PayAdmit.
-    Параметры:
-    - blacklist (list): список объектов для черного списка.
-    Возвращает:
-    - Результат обновления черного списка.
-    Исключения:
-    - HTTPException: если произошла ошибка при обновлении черного списка.
-    """
-    try:
-        return await payadmit_service.update_blacklist(blacklist)
-    except HTTPException as e:
-        raise e
-
-
-@router.post("/whitelist")
-async def update_whitelist(
-    whitelist: list, payadmit_service: PayAdmitService = Depends()
-):
-    """
-    Обновляет белый список через API PayAdmit.
-    Параметры:
-    - whitelist (list): список объектов для белого списка.
-    Возвращает:
-    - Результат обновления белого списка.
-    Исключения:
-    - HTTPException: если произошла ошибка при обновлении белого списка.
-    """
-    try:
-        return await payadmit_service.update_whitelist(whitelist)
+        return await payadmit_service.get_balance(user)
     except HTTPException as e:
         raise e
